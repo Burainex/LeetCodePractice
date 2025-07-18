4113c80da06577e717dc828746ec184628ad4ca5
Burainex <burainex@gmail.com>
2025-07-18 18:37:15 +0700

    876. Middle of the Linked List

---
+++ 0876_MiddleOfTheLinkedList.kt
@@ -0,0 +1,46 @@
+package com.burainex.leetcodesolutions
+
+/*
+ * 876. Middle of the Linked List
+ * URL: https://leetcode.com/problems/middle-of-the-linked-list/description/
+ *
+ * Description:
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ * If there are two middle nodes, return the second middle node.
+ *
+ * Constraints:
+ * The number of nodes in the list is in the range [1, 100].
+ * 1 <= Node.val <= 100
+ *
+ * Example:
+ * Input: head = [1,2,3,4,5,6]
+ * Output: [4,5,6]
+ * Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ */
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun middleNode(head: ListNode?): ListNode? {
+    var slow = head
+    var fast = head
+
+    while (fast?.next != null) {
+        slow = slow?.next
+        fast = fast.next?.next
+    }
+
+    return slow
+}
+
+fun main() {
+    val head = ListNode(1)
+    head.next = ListNode(2)
+    head.next?.next = ListNode(3)
+    head.next?.next?.next = ListNode(4)
+    head.next?.next?.next?.next = ListNode(5)
+    head.next?.next?.next?.next?.next = ListNode(6)
+
+    print(middleNode(head)?.`val`)
+}
