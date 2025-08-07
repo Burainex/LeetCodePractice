010b988d54411cf998a712307dda4483a87f1b27
Burainex <burainex@gmail.com>
2025-08-07 08:21:36 +0700

    724. Find Pivot Index

---
+++ 0724_FindPivotIndex.kt
@@ -0,0 +1,44 @@
+package com.burainex.leetcodesolutions
+
+/*
+ * 724. Find Pivot Index
+ * URL: https://leetcode.com/problems/find-pivot-index/description/
+ *
+ * Description:
+ * Given an array of integers nums, calculate the pivot index of this array.
+ * The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+ * If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+ * Return the leftmost pivot index. If no such index exists, return -1.
+ *
+ * Constraints:
+ * 1 <= nums.length <= 10^4
+ * -1000 <= nums[i] <= 1000
+ *
+ * Example:
+ * Input: nums = [2,1,-1]
+ * Output: 0
+ * Explanation:
+ * The pivot index is 0.
+ * Left sum = 0 (no elements to the left of index 0)
+ * Right sum = nums[1] + nums[2] = 1 + -1 = 0
+ */
+
+fun pivotIndex(nums: IntArray): Int {
+    var left = 0
+    var right = nums.sum()
+
+    nums.forEachIndexed { index, num ->
+        right -= num
+        if (left == right) return index
+        left += num
+    }
+
+    return -1
+}
+
+fun main() {
+    val example = intArrayOf(2, 1, -1)
+    print(
+        pivotIndex(example)
+    )
+}
