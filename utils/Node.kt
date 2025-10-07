165a591147233db05c2d96c267d6f07eafcad306
Burainex <burainex@gmail.com>
2025-10-07 09:07:58 +0700

    - Update Node class for "430. Flatten a Multilevel Doubly Linked List"

--- utils/Node.kt
+++ utils/Node.kt
@@ -1,6 +1,8 @@
 package com.burainex.leetcodesolutions.utils
 
 class Node(var `val`: Int) {
+    var prev: Node? = null
     var next: Node? = null
+    var child: Node? = null
     var random: Node? = null
 }
